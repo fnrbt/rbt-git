6bcae0d8f9979451431a5497e3617dc7b13914c7
fn <fn@cardillion.com>
2026-08-23 16:57:07 -0700

    build: select package actions from ref context

--- .forge/actions.lua
+++ .forge/actions.lua
@@ -1,8 +1,15 @@
 local publish = [[set -euo pipefail; python3 .forge/publish]]
 
 return {
+    select = function(ctx)
+        if string.match(ctx.ref_name, "^refs/tags/v") then
+            return { "publish" }
+        end
+        return {}
+    end,
     publish = function(_)
         return {
+            output = "package",
             base = publish,
             packages = {
                 publish = {
